8a5364622a311f777b0b838d3717da30ec3c6cc0
PythonMonty <andrewnovosel90@gmail.com>
2025-01-06 13:14:29 -0500

    Refactor TodoList and List components to improve functionality and display completed tasks. Removed unnecessary comments, updated handleComplete function, and enhanced List component to filter and display completed todos.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import { React, useState, useEffect } from 'react';
-/*
-  "React" is a library that helps you build user interfaces
-  "useState" is a React tool that helps your component remember information even after it updates
-  "useEffect" is a React tool that lets you perform "side effects" in your components - side effects are operations that happen "on the side" (e.g. fetching data from an API)
-*/
+
 import axios from 'axios'; // "axios" is a library that makes it easier to make HTTP requests, such as GET, POST, PUT, and DELETE requests
 import EditTodo from './components/Todo';
 import List from './components/List';
@@ -19,19 +15,9 @@ const secondaryUrl = 'http://localhost:8000';
 
 
 function TodoList() { // Declaring a function called TodoList with no parameters as input
-// In React, "TodoList" is a component
   const [todos, setTodos] = useState([]); // Creates a list to store multiple todos
-  /*
-    "todos" is a variable that will store the list of todos - like a box that holds todos
-    "setTodos" is a function that lets you update what's stored in "todos"
-    "useState([])" creates a new memory space that starts with an empty array
-  */
   const [inputValue, setInputValue] = useState(''); // Creates a variable to store the new list item
-  /*
-    "inputValue" is a variable that will store the new todo name
-    "setInputValue" is a function that lets you update what's stored in "inputValue"
-    "useState('')" creates a new memory space that starts with an empty string
-  */
+
   const [editId, setEditId] = useState(null);
  
   useEffect(() => {
@@ -88,48 +74,48 @@ function TodoList() { // Declaring a function called TodoList with no parameters
       setEditId(null); // Exits the edit mode, therefore returning to its original state
   }
 
-  // const handleComplete = (id) => {
-  //   const newTodos = [...todos].map(todo => todo.id === id ? { ...todo, completed: true } : todo);
+  const handleComplete = (id) => {
+    const newTodos = [...todos].map(todo => todo.id === id ? { ...todo, completed: true } : todo);
 
-  //   axios.put(`${secondaryUrl}/${id}`, { completed: true })
-  //     .then(() => {
-  //       setTodos(newTodos);
-  //     });
-  // };
-
-  const moveCompletedTodo = (id, name) => {
-    const newTodos = [...todos].map(todo => todo.id === id ? { ...todo, name: name, completed: true } : todo);
-    axios.put(`${secondaryUrl}/${id}`, { name: name, completed: true })
+    axios.put(`${secondaryUrl}/${id}`, { completed: true })
       .then(() => {
         setTodos(newTodos);
       });
-  }
+  };
+
+  // const moveCompletedTodo = (id, name) => {
+  //   const newTodos = [...todos].map(todo => todo.id === id ? { ...todo, name: name, completed: true } : todo);
+  //   axios.put(`${secondaryUrl}/${id}`, { name: name, completed: true })
+  //     .then(() => {
+  //       setTodos(newTodos);
+  //     });
+  // }
 
   return ( // The return statement stops the function and sends back whatever is inside the return statement
-    <div> {/* The div tags define a section in the HTML (JSX) */}
-      <h1>Todo List</h1> {/* The h tags (in this case, h1) define HTML headings */}
+    <div>
+      <h1>To do List</h1>
       <form onSubmit={handleSubmit}>
         <input type="text" value={inputValue} onChange={handleChange} />
-        <button><MdAdd /></button> {/* The button tag is used to trigger an action */}
+        <button><MdAdd /></button>
       </form>
       <h5>Incompleted</h5>
-      <ul> {/* The ul tags define an unordered list of items */}
+      <ul>
         {todos.map((todo) => (
           <li key={todo.id} style={todo.completed ? {textDecoration: "line-through"} : {}}>
             {editId === todo.id ? (
               <EditTodo todo={todo} onSave={handleUpdate} onCancel={() => setEditId(null)} />
             ) : (
               <>
-                {todo.name} {/* The todo.name is the name of the todo */}
+                {todo.name}
                 <button onClick={() => setEditId(todo.id)}><MdEdit /></button>
                 <button onClick={() => handleDelete(todo.id)}><FaTrash /></button>
-                <button onClick={() => moveCompletedTodo(todo.id)}><FcCheckmark /></button>
+                <button onClick={() => handleComplete(todo.id)}><FcCheckmark /></button>
               </>
             )}
           </li>
         ))}
       </ul>
-      <List />
+      <List todos={todos} />
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { FaTrash } from "react-icons/fa";
 
-function List({todo, completed}) {
-   // const [completedList, setcompletedList] = useState('');
+// Take in the array of todos by name and add it to the completed list
+// In the todos array, you need to filter out the todo list items that are complete by using a conditional statement
+
+function List(props) { // props.todos
+    const completed = props.todos.filter(todo => todo.completed);
+
+    // console.log(completed);
 
     return (
         <div>
             <h5>Completed</h5>
             <ul>
-                <li></li>
+                {completed.map(complete => {
+                    return <li>{complete}</li>
+                })}
             </ul>
         </div>
     )
